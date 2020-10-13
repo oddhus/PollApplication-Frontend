b909ded44ba094b0bdabdcfd9f594acbcbf71f27
oddhus <huseby.oddmund@gmail.com>
2020-10-13 20:54:54 +0200

    refractor

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const AccountPage = () => {
-  return (
-    <div>
-      AccountPage
-    </div>
-  )
-}
+  return <div>AccountPage</div>;
+};
