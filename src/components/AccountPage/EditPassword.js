60f091d6ce2b31c4b86b59d25e663bf84f83cf0f
oddhus <huseby.oddmund@gmail.com>
2020-10-14 15:11:58 +0200

    Added Edit Password component

---
+++ src/components/AccountPage/EditPassword.js
@@ -0,0 +1,57 @@
+import { Grid, TextField } from "@material-ui/core";
+import React, { useRef } from "react";
+import { Controller } from "react-hook-form";
+
+export const EditPassword = ({ control, errors, watch }) => {
+  const password = useRef({});
+  password.current = watch("password", "");
+  return (
+    <React.Fragment>
+      <Grid item xs={12} sm={6}>
+        <Controller
+          as={TextField}
+          rules={{
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be atleast 8 chars",
+            },
+          }}
+          control={control}
+          defaultValue=""
+          size="small"
+          variant="outlined"
+          required
+          name="password"
+          label="Password"
+          type="password"
+          fullWidth
+          error={!!errors.password}
+          helperText={errors.password ? errors.password.message : ""}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Controller
+          as={TextField}
+          rules={{
+            validate: (value) =>
+              value === password.current || "The passwords do not match",
+          }}
+          control={control}
+          defaultValue=""
+          variant="outlined"
+          required
+          name="repeatpassword"
+          label="Repeat Password"
+          type="password"
+          size="small"
+          fullWidth
+          error={!!errors.repeatpassword}
+          helperText={
+            errors.repeatpassword ? errors.repeatpassword.message : ""
+          }
+        />
+      </Grid>
+    </React.Fragment>
+  );
+};
