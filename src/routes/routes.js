95b1794f4f29789cefc0966ac7b1188e5fe46199
oddhus <huseby.oddmund@gmail.com>
2020-10-14 09:01:01 +0200

    small update

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,11 +8,13 @@ import { CreateEditPollPage } from "../pages/CreateEditPollPage";
 import { AccountPage } from "../pages/AccountPage";
 import { AdminPage } from "../pages/AdminPage";
 import { PublicPollsPage } from "../pages/PublicPollsPage";
+import { VotePage } from "../pages/VotePage";
 
 export const UnauthenticatedAppRoutes = () => {
   return (
     <React.Fragment>
       <Route exact path="/" component={LandingPage} />
+      <Route exact path="/vote/:pollId" component={VotePage} />
       <Route exact path="/login" component={LoginPage} />
       <Route exact path="/register" component={RegisterPage} />
     </React.Fragment>
@@ -22,9 +24,11 @@ export const UnauthenticatedAppRoutes = () => {
 export const AuthenticatedAppRoutes = () => {
   return (
     <React.Fragment>
+      <Route exact path="/" component={LandingPage} />
       <Route exact path="/polls" component={UserPollsPage} />
-      <Route path="/create" component={CreateEditPollPage} />
       <Route path="/polls/:pollId" component={CreateEditPollPage} />
+      <Route path="/vote/:pollId" component={VotePage} />
+      <Route path="/create" component={CreateEditPollPage} />
       <Route path="/public" component={PublicPollsPage} />
       <Route path="/account" component={AccountPage} />
     </React.Fragment>
