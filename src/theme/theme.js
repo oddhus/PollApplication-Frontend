a98d6689f9b421e9bdafc12efbdc1c68ab9659b7
oddhus <huseby.oddmund@gmail.com>
2020-10-14 11:45:00 +0200

    added typography style

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,20 +1,20 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import red from "@material-ui/core/colors/red";
-import amber from '@material-ui/core/colors/amber';
-
+import amber from "@material-ui/core/colors/amber";
+import grey from "@material-ui/core/colors/grey";
 
 // Create a theme instance.
 //https://v3.material-ui.com/style/color/#color-tool
 const theme = createMuiTheme({
-    palette: {
-      primary: {
-        main: '#388e3c',
-      },
-      secondary: amber,
-      error: {
-        main: red.A400,
-      },
+  palette: {
+    primary: {
+      main: "#388e3c",
     },
+    secondary: amber,
+    error: {
+      main: red.A400,
+    },
+  },
   typography: {
     h2: {
       color: "white",
@@ -22,7 +22,12 @@ const theme = createMuiTheme({
       fontWeight: 300,
       fontSize: "2rem",
       lineHeight: 1.5,
-    }
+    },
+    subtitle1: {
+      color: grey.A700,
+      fontWeight: 700,
+      lineHeight: 1.5,
+    },
   },
 });
 
