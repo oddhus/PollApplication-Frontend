dfb819f3d7669de590b1a788e355b7495d2f620f
Oddmund Huseby <51832618+oddhus@users.noreply.github.com>
2020-10-15 16:59:50 +0200

    added field for old password

--- src/components/AccountPage/EditPassword.js
+++ src/components/AccountPage/EditPassword.js
@@ -11,7 +11,30 @@ export const EditPassword = ({ control, errors, watch }) => {
         <Controller
           as={TextField}
           rules={{
-            required: "Password is required",
+            required: "Old password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be atleast 8 chars",
+            },
+          }}
+          control={control}
+          defaultValue=""
+          size="small"
+          variant="outlined"
+          required
+          name="oldpassword"
+          label="Old Password"
+          type="password"
+          fullWidth
+          error={!!errors.oldpassword}
+          helperText={errors.oldpassword ? errors.oldpassword.message : ""}
+        />
+      </Grid>
+      <Grid item xs={12} sm={8}>
+        <Controller
+          as={TextField}
+          rules={{
+            required: "New password is required",
             minLength: {
               value: 8,
               message: "Password must be atleast 8 chars",
