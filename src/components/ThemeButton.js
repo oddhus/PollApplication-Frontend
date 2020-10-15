000bb868f55471a24c20bc541f92de474239d1e0
JennStro <jennys1994@gmail.com>
2020-10-15 21:23:48 +0200

    Add spacing between components

--- src/components/ThemeButton.js
+++ src/components/ThemeButton.js
@@ -2,6 +2,5 @@ import React from "react";
 import {Button} from "@material-ui/core";
 
 export const ThemeButton = (props) => {
-
-    return <Button variant="contained" color="primary" type="submit">{props.value.toString()}</Button>
+    return <Button variant="contained" color="primary" type="submit">{props.text.toString()}</Button>;
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {ThemeButton} from "../components/ThemeButton";
 import { useForm } from "react-hook-form";
-import {TextField, Container, Grid} from "@material-ui/core";
+import {TextField, Container, Grid, Box} from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 
 export const LoginPage = () => {
@@ -12,16 +12,22 @@ export const LoginPage = () => {
   return (
       <Container className={classes.container} maxWidth="xs">
           <form onSubmit={handleSubmit(onSubmit)}>
-              <Grid>
-                  <TextField label="Username" name="usernameLoginField" inputRef={register( {required: true})} />
-                  {errors.usernameLoginField && <span>Du må skrive inn brukernavn!</span>}
-              </Grid>
-              <Grid>
-                  <TextField label="Password" name="passwordLoginField" inputRef={register({ required: true })} type="password" />
-                  {errors.passwordLoginField && <span>Du må skrive inn passord!</span>}
-              </Grid>
-              <Grid>
-                   <ThemeButton value={"Log in"} />
+              <Grid container
+                    direction="column"
+                    justify="space-between"
+                    alignItems="center"
+              >
+                  <Box>
+                      <TextField label="Username" name="usernameLoginField" inputRef={register( {required: true})} />
+                      {errors.usernameLoginField && <span>Du må skrive inn brukernavn!</span>}
+                  </Box>
+                  <Box marginTop={5}>
+                      <TextField label="Password" name="passwordLoginField" inputRef={register({ required: true })} type="password" />
+                      {errors.passwordLoginField && <span>Du må skrive inn passord!</span>}
+                  </Box>
+                  <Box marginTop={5}>
+                     <ThemeButton text={"log in"}/>
+                  </Box>
               </Grid>
           </form>
       </Container>
