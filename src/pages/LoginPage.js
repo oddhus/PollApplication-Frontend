06027c6d605f0f14025428e24fba070b5ec6b0cc
JennStro <jennys1994@gmail.com>
2020-10-15 20:29:02 +0200

    Create button according to style

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useForm } from "react-hook-form";
-import {TextField, Container, Grid} from "@material-ui/core";
+import {TextField, Container, Grid, Button} from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 
 export const LoginPage = () => {
@@ -11,11 +11,17 @@ export const LoginPage = () => {
   return (
       <Container className={classes.container} maxWidth="xs">
           <form onSubmit={handleSubmit(onSubmit)}>
-              <Grid><TextField label="Username" name="usernameLoginField" inputRef={register( {required: true})} /></Grid>
-              <Grid><TextField label="Password" name="passwordLoginField" inputRef={register({ required: true })} type="password" /></Grid>
-              {errors.usernameLoginField && <span>Du må skrive inn brukernavn!</span>}
-              {errors.passwordLoginField && <span>Du må skrive inn passord!</span>}
-              <input type="submit" />
+              <Grid>
+                  <TextField label="Username" name="usernameLoginField" inputRef={register( {required: true})} />
+                  {errors.usernameLoginField && <span>Du må skrive inn brukernavn!</span>}
+              </Grid>
+              <Grid>
+                  <TextField label="Password" name="passwordLoginField" inputRef={register({ required: true })} type="password" />
+                  {errors.passwordLoginField && <span>Du må skrive inn passord!</span>}
+              </Grid>
+              <Grid>
+                  <Button variant="contained" color="primary" className={classes.button} type="submit">Submit</Button>
+              </Grid>
           </form>
       </Container>
   )
