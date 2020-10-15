803469d862aed0a86ea83e3c1e8806392d6e15c9
JennStro <jennys1994@gmail.com>
2020-10-15 19:23:07 +0200

    Add login field

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { useForm } from "react-hook-form";
 
-const { register, handleSubmit, watch, errors } = useForm();
-const onSubmit = data => console.log(data);
-
 export const LoginPage = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => console.log(data);
+
   return (
       <form onSubmit={handleSubmit(onSubmit)}>
-          <input name="example" defaultValue="test" ref={register} />
-          <input name="exampleRequired" ref={register({ required: true })} />
-          {errors.exampleRequired && <span>This field is required</span>}
+          <input name="usernameLoginField" defaultValue="Username" ref={register( {required: true})} />
+          <input name="passwordLoginField" ref={register({ required: true })} />
+          {errors.usernameLoginField && <span>Du må skrive inn brukernavn!</span>}
+          {errors.passwordLoginField && <span>Du må skrive inn passord!</span>}
           <input type="submit" />
       </form>
   )
