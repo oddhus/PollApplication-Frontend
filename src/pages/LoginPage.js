b87266cfb74f36a1a27e5f188496eafaa7a36664
JennStro <jennys1994@gmail.com>
2020-10-16 14:48:02 +0200

    Forat with Prettier

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,44 +1,75 @@
-import React from 'react'
-import {ThemeButton} from "../components/ThemeButton";
+import React from "react";
+import { ThemeButton } from "../components/ThemeButton";
 import { useForm } from "react-hook-form";
-import {TextField, Container, Grid, Box, Link} from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
-import { Link as RouterLink } from 'react-router-dom';
-import {login} from "../mock/auth";
+import { TextField, Container, Grid, Box, Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link as RouterLink } from "react-router-dom";
+import { login } from "../mock/auth";
 
 export const LoginPage = () => {
-    const { register, handleSubmit, errors } = useForm();
-    const onSubmit = data => {
-        console.log(data);
-        login();
-    }
-    const classes = useStyles();
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    login();
+  };
+  const classes = useStyles();
 
   return (
-      <Container className={classes.container} maxWidth="xs">
-          <form onSubmit={handleSubmit(onSubmit)}>
-              <Grid container
-                    direction="column"
-                    justify="space-between"
-                    alignItems="center"
-              >
-                  <Box><TextField label="Username" name="usernameLoginField" inputRef={register( {required: true})} /></Box>
-                  <Box marginTop={2}><TextField label="Password" name="passwordLoginField" inputRef={register({ required: true })} type="password" /></Box>
-                  <Box marginTop={5}><ThemeButton text={"log in"}/></Box>
-                  <Box marginTop={2}><Link component={RouterLink} to={"/register"}>Create an account.</Link></Box>
-                  <Box> <Link component={RouterLink} to={"/login"} onClick={() => alert(JSON.stringify("We sent you a new password to: mail"))}>Send me a new password.</Link></Box>
-                  <Box marginTop={5}>
-                      {errors.usernameLoginField && <span>Du må gi et brukernavn!</span>}
-                      {errors.passwordLoginField && <span>     Du må gi et passord!</span>}
-                  </Box>
-              </Grid>
-          </form>
-      </Container>
-  )
-}
+    <Container className={classes.container} maxWidth="xs">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid
+          container
+          direction="column"
+          justify="space-between"
+          alignItems="center"
+        >
+          <Box>
+            <TextField
+              label="Username"
+              name="usernameLoginField"
+              inputRef={register({ required: true })}
+            />
+          </Box>
+          <Box marginTop={2}>
+            <TextField
+              label="Password"
+              name="passwordLoginField"
+              inputRef={register({ required: true })}
+              type="password"
+            />
+          </Box>
+          <Box marginTop={5}>
+            <ThemeButton text={"log in"} />
+          </Box>
+          <Box marginTop={2}>
+            <Link component={RouterLink} to={"/register"}>
+              Create an account.
+            </Link>
+          </Box>
+          <Box>
+            {" "}
+            <Link
+              component={RouterLink}
+              to={"/login"}
+              onClick={() =>
+                alert(JSON.stringify("We sent you a new password to: mail"))
+              }
+            >
+              Send me a new password.
+            </Link>
+          </Box>
+          <Box marginTop={5}>
+            {errors.usernameLoginField && <span>Du må gi et brukernavn!</span>}
+            {errors.passwordLoginField && <span> Du må gi et passord!</span>}
+          </Box>
+        </Grid>
+      </form>
+    </Container>
+  );
+};
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        padding: theme.spacing(3),
-    },
+  container: {
+    padding: theme.spacing(3),
+  },
 }));
