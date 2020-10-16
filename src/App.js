7b73bf00c6a1236212ca45160806bb2ed7f04389
Oddmund Huseby <51832618+oddhus@users.noreply.github.com>
2020-10-16 10:50:32 +0200

    check user state

--- src/App.js
+++ src/App.js
@@ -7,23 +7,23 @@ import {
   AuthenticatedAppRoutes,
   UnauthenticatedAppRoutes,
 } from "./routes/routes";
+import useUser from "./data/use-user";
 
 export const App = () => {
-  const isAuthenticated = true;
-  const isAdmin = true;
+  const { loggedOut, user } = useUser();
 
   return (
     <BrowserRouter>
       <Navbar />
       <Container maxWidth={"md"}>
         <Switch>
-          {isAuthenticated ? (
+          {!loggedOut ? (
             <AuthenticatedAppRoutes />
           ) : (
             <UnauthenticatedAppRoutes />
           )}
         </Switch>
-        <Switch>{isAdmin ? <AdminAppRoutes /> : null}</Switch>
+        <Switch>{user && user.admin ? <AdminAppRoutes /> : null}</Switch>
       </Container>
     </BrowserRouter>
   );
