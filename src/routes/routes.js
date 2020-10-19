ddc86b1da2e50b74eda39c6f0815e18557ed99fa
Severinzz <36744838+Severinzz@users.noreply.github.com>
2020-10-19 18:58:53 +0200

    Finished no match page
    Added no match page
    Tile components
    Updated routes to contain no match page.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,15 +10,20 @@ import { AdminPage } from "../pages/AdminPage";
 import { PublicPollsPage } from "../pages/PublicPollsPage";
 import { VotePage } from "../pages/VotePage";
 import { ResultPage } from "../pages/ResultPage";
+import { NoMatch } from "../pages/NoMatch"
+import { Switch } from "react-router-dom";
 
 export const UnauthenticatedAppRoutes = () => {
   return (
     <React.Fragment>
-      <Route exact path="/" component={LandingPage} />
-      <Route path="/vote/:pollId" component={VotePage} />
-      <Route path="/result/:pollId" component={ResultPage} />
-      <Route exact path="/login" component={LoginPage} />
-      <Route exact path="/register" component={RegisterPage} />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/vote/:pollId" component={VotePage} />
+        <Route path="/result/:pollId" component={ResultPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+        <Route component={NoMatch} />
+      </Switch>
     </React.Fragment>
   );
 };
@@ -26,14 +31,17 @@ export const UnauthenticatedAppRoutes = () => {
 export const AuthenticatedAppRoutes = () => {
   return (
     <React.Fragment>
-      <Route exact path="/" component={LandingPage} />
-      <Route exact path="/polls" component={UserPollsPage} />
-      <Route path="/polls/:pollId" component={CreateEditPollPage} />
-      <Route path="/vote/:pollId" component={VotePage} />
-      <Route path="/result/:pollId" component={ResultPage} />
-      <Route path="/create" component={CreateEditPollPage} />
-      <Route path="/public" component={PublicPollsPage} />
-      <Route path="/account" component={AccountPage} />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/polls" component={UserPollsPage} />
+        <Route path="/polls/:pollId" component={CreateEditPollPage} />
+        <Route path="/vote/:pollId" component={VotePage} />
+        <Route path="/result/:pollId" component={ResultPage} />
+        <Route path="/create" component={CreateEditPollPage} />
+        <Route path="/public" component={PublicPollsPage} />
+        <Route path="/account" component={AccountPage} />
+        <Route component={NoMatch} />
+      </Switch>
     </React.Fragment>
   );
 };
@@ -41,7 +49,10 @@ export const AuthenticatedAppRoutes = () => {
 export const AdminAppRoutes = () => {
   return (
     <React.Fragment>
+      <Switch>
       <Route path="/admin" component={AdminPage} />
+      {/* <Route component={NoMatch} /> */}
+      </Switch>
     </React.Fragment>
   );
 };
