794e8a08e1bfdb0e71a52b037a2ec19f9ebefadd
JennStro <jennys1994@gmail.com>
2020-10-19 19:10:16 +0200

    Change from norwegian to english

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -37,16 +37,41 @@ export const RegisterPage = () => {
             <TextField
               label="Username"
               name="username"
-              inputRef={register({ required: "Du må gi et brukernavn!" })}
+              inputRef={register({ required: "You have to give a username." })}
               error={!!errors.username}
               helperText={errors.username ? errors.username.message : ""}
             />
           </Grid>
           <Grid item>
             <TextField
+              label="Email"
+              name="email"
+              inputRef={register({
+                required: "You have to give an email-address.",
+                pattern: {
+                  value: /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/,
+                  message: "Your email is not valid.",
+                },
+              })}
+              error={!!errors.email}
+              helperText={errors.email ? errors.email.message : ""}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
               label="Password"
               name="password"
-              inputRef={register({ required: "Du må gi et passord!" })}
+              inputRef={register({ required: "You have to give a password!" })}
+              error={!!errors.password}
+              helperText={errors.password ? errors.password.message : ""}
+              type="password"
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              label="Password"
+              name="secondPassword"
+              inputRef={register({ required: "You have to give a password!" })}
               error={!!errors.password}
               helperText={errors.password ? errors.password.message : ""}
               type="password"
@@ -54,12 +79,12 @@ export const RegisterPage = () => {
           </Grid>
           <Grid item>
             <ThemeButton>
-              {isSubmitting ? <ThemeCircularProgress /> : "Log in"}
+              {isSubmitting ? <ThemeCircularProgress /> : "Registrer"}
             </ThemeButton>
           </Grid>
           <Grid item>
             <Link component={RouterLink} to={"/login"}>
-              Har du allerede en bruker? Logg på.
+              Already got an account? Sign in instead.
             </Link>
           </Grid>
         </Grid>
