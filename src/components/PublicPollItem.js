bd064a34de239345348b2f39dd4267c2534281be
JennStro <jennys1994@gmail.com>
2020-11-03 14:18:05 +0100

    Get result when pressing result button

--- src/components/PublicPollItem.js
+++ src/components/PublicPollItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import { Grid, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
@@ -6,136 +6,149 @@ import moment from "moment";
 import { ThemeButton } from "./ThemeButton";
 import { ThemeCircularProgress } from "./ThemeCircularProgress";
 import { makeStyles } from "@material-ui/core/styles";
+import { ResultChart } from "./ResultChart";
+import { ResultModal } from "./ResultModal";
 
-function onDisplayResults() {
-  console.log("Results");
-}
+const useStyles = makeStyles((theme) => ({
+  buttonContainer: {
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    [theme.breakpoints.down("xs")]: {
+      paddingBottom: theme.spacing(1),
+    },
+  },
+  finishedInfo: {
+    fontStyle: "italic",
+    paddingTop: 10,
+    paddingLeft: 20,
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: 0,
+      paddingLeft: 10,
+    },
+  },
+  linkContainer: {
+    textDecoration: "none",
+  },
+  pollContainer: {
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      height: 65,
+    },
+  },
+  title: {
+    color: "#323949",
+    paddingLeft: 10,
+    paddingTop: 10,
+    fontSize: 24,
+  },
+  question: {
+    paddingLeft: 10,
+    marginBottom: 8,
+  },
+}));
 
 export const PublicPollItem = ({ poll }) => {
-  const useStyles = makeStyles((theme) => ({
-    buttonContainer: {
-      paddingLeft: theme.spacing(1),
-      paddingRight: theme.spacing(1),
-      [theme.breakpoints.down("xs")]: {
-        paddingBottom: theme.spacing(1),
-      },
-    },
-    finishedInfo: {
-      fontStyle: "italic",
-      paddingTop: 10,
-      paddingLeft: 20,
-      [theme.breakpoints.down("sm")]: {
-        paddingTop: 0,
-        paddingLeft: 10,
-      },
-    },
-    linkContainer: {
-      textDecoration: "none",
-    },
-    pollContainer: {
-      marginBottom: theme.spacing(2),
-      [theme.breakpoints.up("sm")]: {
-        height: 65,
-      },
-    },
-    title: {
-      color: "#323949",
-      paddingLeft: 10,
-      paddingTop: 10,
-      fontSize: 24,
-    },
-    question: {
-      paddingLeft: 10,
-      marginBottom: 8,
-    },
-  }));
+  const [openResults, setOpenResults] = useState(false);
+  function onDisplayResults() {
+    console.log("Results");
+    setOpenResults(true);
+  }
 
   const classes = useStyles();
   let isLoading = false;
   return (
-    <Grid item className={classes.pollContainer}>
-      <Paper>
-        <Grid item container direction="row">
-          <Grid item container direction="column" sm={6} md={8}>
-            <Grid
-              item
-              container
-              component={poll.category !== 0 ? Link : undefined}
-              to={
-                poll.category !== 0
-                  ? { pathname: `/result/${poll.id}`, state: poll }
-                  : undefined
-              }
-              className={classes.linkContainer}
-            >
+    <React.Fragment>
+      <ResultModal
+        open={openResults}
+        setOpen={setOpenResults}
+        header={poll.question}
+      >
+        <ResultChart id={poll.id} />
+      </ResultModal>
+      <Grid item className={classes.pollContainer}>
+        <Paper>
+          <Grid item container direction="row">
+            <Grid item container direction="column" sm={6} md={8}>
+              <Grid
+                item
+                container
+                component={poll.category !== 0 ? Link : undefined}
+                to={
+                  poll.category !== 0
+                    ? { pathname: `/result/${poll.id}`, state: poll }
+                    : undefined
+                }
+                className={classes.linkContainer}
+              >
+                <Grid item>
+                  <Typography
+                    variant="h5"
+                    className={classes.title}
+                    color="textSecondary"
+                    noWrap
+                  >
+                    {poll.question}
+                  </Typography>
+                </Grid>
+                {poll.category === 1 && (
+                  <Grid item container sm={12} md={4} alignItems="center">
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      className={classes.finishedInfo}
+                    >
+                      Time remaining:{" "}
+                      {moment(poll.startTime)
+                        .add(poll.pollDuration, "seconds")
+                        .fromNow(true)}
+                    </Typography>
+                  </Grid>
+                )}
+                {poll.category === 2 && (
+                  <Grid item container sm={12} md={4} alignItems="center">
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      className={classes.finishedInfo}
+                    >
+                      Finished{" "}
+                      {moment(poll.startTime)
+                        .add(poll.pollDuration, "seconds")
+                        .fromNow()}
+                    </Typography>
+                  </Grid>
+                )}
+              </Grid>
               <Grid item>
                 <Typography
-                  variant="h5"
-                  className={classes.title}
+                  className={classes.question}
                   color="textSecondary"
                   noWrap
                 >
-                  {poll.question}
+                  Poll id: {poll.id}
                 </Typography>
               </Grid>
-              {poll.category === 1 && (
-                <Grid item container sm={12} md={4} alignItems="center">
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    className={classes.finishedInfo}
-                  >
-                    Time remaining:{" "}
-                    {moment(poll.startTime)
-                      .add(poll.pollDuration, "seconds")
-                      .fromNow(true)}
-                  </Typography>
-                </Grid>
-              )}
-              {poll.category === 2 && (
-                <Grid item container sm={12} md={4} alignItems="center">
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    className={classes.finishedInfo}
-                  >
-                    Finished{" "}
-                    {moment(poll.startTime)
-                      .add(poll.pollDuration, "seconds")
-                      .fromNow()}
-                  </Typography>
+            </Grid>
+            <Grid
+              item
+              container
+              xs={12}
+              sm={6}
+              md={4}
+              alignContent="center"
+              justify="flex-end"
+            >
+              {poll.category !== 0 && (
+                <Grid item className={classes.buttonContainer}>
+                  <ThemeButton onClick={() => onDisplayResults()}>
+                    {isLoading ? <ThemeCircularProgress /> : "Results"}
+                  </ThemeButton>
                 </Grid>
               )}
             </Grid>
-            <Grid item>
-              <Typography
-                className={classes.question}
-                color="textSecondary"
-                noWrap
-              >
-                Poll id: {poll.id}
-              </Typography>
-            </Grid>
           </Grid>
-          <Grid
-            item
-            container
-            xs={12}
-            sm={6}
-            md={4}
-            alignContent="center"
-            justify="flex-end"
-          >
-            {poll.category !== 0 && (
-              <Grid item className={classes.buttonContainer}>
-                <ThemeButton onClick={() => onDisplayResults()}>
-                  {isLoading ? <ThemeCircularProgress /> : "Results"}
-                </ThemeButton>
-              </Grid>
-            )}
-          </Grid>
-        </Grid>
-      </Paper>
-    </Grid>
+        </Paper>
+      </Grid>
+    </React.Fragment>
   );
 };
